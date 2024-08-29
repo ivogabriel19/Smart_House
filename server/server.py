340d68cd1e7e61795ede3bafedc5cde30a6b2550
Ivo Gabriel <ivo.degange@ort.edu.ar>
2024-08-29 15:28:30 -0300

    fix server

--- server/server.py
+++ server/server.py
@@ -100,4 +100,4 @@ def handle_disconnect():
 
 if __name__ == '__main__':
 #    app.run(host='0.0.0.0', port=5000)
-    socketio.run(app, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=False)
