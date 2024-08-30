76d3dca1c2254e9a366779a716ddaa5203a07d5e
Ivo Gabriel <ivo.degange@ort.edu.ar>
2024-08-30 15:50:11 -0300

    prueba front

---
+++ cod-fdp/components/ESP-card/script.js
@@ -0,0 +1,7 @@
+const dummy = {
+                "Dummy_ESP": {
+                    "IP": "0.0.0.1",
+                    "MAC" : "00:00:00:00:00:01",
+                    "status": "non-existent"
+                }
+            }
