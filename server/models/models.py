b657f587dc0dc670280023cba57ac94028b574c6
ivogabriel19 <ivodegange@gmail.com>
2024-10-08 02:46:22 -0300

    modelos ESP

--- server/models/models.py
+++ server/models/models.py
@@ -1,17 +1,33 @@
 import time
-
+# Clase base para ESP
 class ESP:
-    def __init__(self, id, ip, mac, status, type, data):
-        self.ID = id,
-        self.IP = ip,
-        self.MAC = mac,
-        self.status = status,
-        self.type = type,
-        self.last_seen = time.time(),
+    def __init__(self, device_id, device_ip, device_mac, device_type):
+        self.ID = device_id
+        self.IP = device_ip
+        self.MAC = device_mac
+        self.status = "Online"
+        self.last_seen = time.time()
+        self.type = device_type
         self.data = {}
+        self.events = []
+
+    def actualizar_last_seen(self):
+        self.last_seen = time.time()
     
     def __str__(self):
         return f"{self.ID} {self.IP} {self.MAC} {self.status} {self.type} {self.last_seen} {self.data} "
+
+# Clase Sensor que hereda de ESP
+class Sensor(ESP):
+    def __init__(self, device_id, device_ip, device_mac):
+        super().__init__(device_id, device_ip, device_mac, device_type="Sensor")
+        self.data = {"temperatura": "", "humedad": ""}
+
+# Clase Actuador que hereda de ESP
+class Actuador(ESP):
+    def __init__(self, device_id, device_ip, device_mac):
+        super().__init__(device_id, device_ip, device_mac, device_type="Actuador")
+        self.data = {"switch": "OFF"}   
 
 class Event:
     def __init__(self, id, alias, type, data, action):
